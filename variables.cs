ecb638a08e7d6a5a16563890caa8425d30cb5e0e
Franz Tapia Chaca <franz.tapia-chaca@outlook.com>
2022-04-22 09:02:02 +0200

    Create variables.cs

---
+++ variables.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+public class Class1
+{
+	public Class1()
+	{
+		string phrase = "Giraffe Academy";
+		char grade = 'A';
+		int age = 30;
+		double gpa = 30.2; // float, double, decimal (least to most precise)
+		bool isMale = true;
+		Console.WriteLine("Hello");
+
+		Console.ReadLine();
+
+	}
+}
